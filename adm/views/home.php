15de5bc42800398f5ce6ea3ffd2a20e9a891d1c7
OkaOka-502 <ciciokta819@gmail.com>
2022-04-10 23:33:51 +0700

    first commit

---
+++ adm/views/home.php
@@ -0,0 +1,8 @@
+<div class="row">
+  <div class="col-md-12">
+    <h1>HOME.</h1>
+    <p>halao slemat datang!</p>
+    <h3>ini halaman home yang ditulis saat ini!!!</h3>
+    <input type="text" class="form-control">
+  </div>
+</div>
